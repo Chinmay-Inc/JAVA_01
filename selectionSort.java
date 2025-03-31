98ef1e6caa24087ce8102c5dd410534045b6f831
CHINMAY <chinmayinchal979@gmail.com>
2025-03-31 13:14:31 +0530

    yo

--- selectionSort.java
+++ selectionSort.java
@@ -3,7 +3,22 @@ public class selectionSort {
     public static void Sort(int[]arr){
         int n=arr.length;
         
-        //for(int i)
+        for(int i=0;i<n-1;i++){
+            int min=i;
+            for(int j=i+1;j<n;j++){
+                if(arr[min]>arr[j]){
+                    min=j;
+                }
+            }
+            int temp=arr[i];
+            arr[i]=arr[min];
+            arr[min]=temp;
+
+
+        }
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
     }
 
     public static void main(String[]args){
